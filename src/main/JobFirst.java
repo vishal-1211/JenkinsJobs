fc7e3a32a973b0071531b5dc4b715ae1ef549397
Arora <vishaarora@deloitte.com>
2023-03-17 13:24:42 +0530

    Sample Job Files

---
+++ src/main/JobFirst.java
@@ -0,0 +1,9 @@
+package main;
+
+public class JobFirst {
+
+	public static void main(String[] args) {
+		System.out.println("From Job First");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author vishaarora
+ *
+ */
+module SampleJenkinsJobs {
+}
