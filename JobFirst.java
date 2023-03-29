c328f548a036aad35b6c63283d24494e47b220c7
Arora <vishaarora@deloitte.com>
2023-03-29 23:31:08 +0530

    Selenium jar file added

--- JobFirst.java
+++ JobFirst.java
@@ -1,7 +1,28 @@
+import java.io.InputStream;
+
 public class JobFirst {
 
 	public static void main(String[] args) {
-		System.out.println("From Job First");
+	  try {
+		System.out.println("Admin App Test Case 1 Triggered...");
+		System.out.println("Admin App Test Case 10 Triggered...");
+		
+		Process proc = Runtime.getRuntime().exec("java -jar HeadlessAutomationJar.jar");
+		proc.waitFor();
+		// Then retreive the process output
+		InputStream in = proc.getInputStream();
+		InputStream err = proc.getErrorStream();
+
+		byte b[]=new byte[in.available()];
+		in.read(b,0,b.length);
+		System.out.println(new String(b));
+
+		byte c[]=new byte[err.available()];
+		err.read(c,0,c.length);
+		System.out.println(new String(c));
+	  } catch (Exception e ) {
+		e.printStackTrace();
+	  }		
 	}
 
 }
