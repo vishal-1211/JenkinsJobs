800e252e17b267dfaa32239f22c320858a50be5f
Vishal Arora <65604476+vishal-1211@users.noreply.github.com>
2023-04-20 12:59:12 +0530

    Updating sysouts

--- JobSecond.java
+++ JobSecond.java
@@ -4,8 +4,9 @@ public class JobSecond {
 
 	public static void main(String[] args) {
 	  try {
-		System.out.println("Customer App Test Case 1 Triggered...");
-		System.out.println("Customer App Test Case 7 Triggered...");
+		System.out.println("IQE-T4 Triggered...");
+		System.out.println("IQE-T2 Triggered...");
+		System.out.println("IQE-T1 Triggered...");
 		
 		Process proc = Runtime.getRuntime().exec("java -jar HeadlessAutomationJar.jar");
 		proc.waitFor();
